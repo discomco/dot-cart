2b4cdf08ddcfccc42561fbfd4328878a10c08673
Rafael Lefever <rafael.lefever@discomco.pl>
2022-10-06 00:22:48 +0200

    added DotCart Assembly

--- cid/pub_pkg.sh
+++ cid/pub_pkg.sh
@@ -1,9 +1,4 @@
 #! /bin/bash
-echo  "branch=$CI_COMMIT_REF_NAME"
-export version=$1
-echo  "branch=$CI_COMMIT_REF_NAME"
-ls -la
-ls -la ./BLD
 for f in ./BLD/*.nupkg; do
    dotnet nuget push "$f" -k "$NUGET_API_KEY" -s "$NUGET_URL"
 done
