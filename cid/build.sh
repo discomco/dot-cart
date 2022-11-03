6c24fd04d6e0f7267d8b9a1a6a999483e9113a5f
Rafael Lefever <rafael.lefever@discomco.pl>
2022-11-03 22:52:11 +0100

    fix long tests

--- cid/build.sh
+++ cid/build.sh
@@ -2,6 +2,6 @@
 dotnet nuget locals --clear all
 dotnet restore --disable-parallel
 rm -rf ./BLD
-dotnet build -o ./BLD -c Release --ignore-failed-sources DotCart.root.sln
+dotnet build -o ./BLD -c Release --no-restore --ignore-failed-sources "$1"
 
 
--- cid/run_unit_tests.sh
+++ cid/run_unit_tests.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-dotnet test --test-adapter-path:. --logger:trx --results-directory ../../TST-RES --verbosity detailed --configuration Debug "$1"
+dotnet test --test-adapter-path:. --logger:trx --results-directory ../../TST-RES --verbosity normal --no-restore --configuration Debug "$1"
