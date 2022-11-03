d8a553a4cb935b9be1da1569023c691ca4b928e1
Rafael Lefever <rafael.lefever@discomco.pl>
2022-11-03 22:57:21 +0100

    fix long tests

--- cid/run_unit_tests.sh
+++ cid/run_unit_tests.sh
@@ -1,2 +1,4 @@
 #! /bin/sh
-dotnet test --test-adapter-path:. --logger:trx --results-directory ../../TST-RES --verbosity normal --no-restore --configuration Debug "$1"
+#dotnet test --test-adapter-path:. --logger:trx --results-directory ../../TST-RES --verbosity normal --no-restore --configuration Debug "$1"
+
+dotnet test --verbosity normal --no-restore "$1"
